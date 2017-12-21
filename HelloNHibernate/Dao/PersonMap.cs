f6fc5b61bc0680cedc288387644786035653f5f4
Tama McGlinn <t.mcglinn@gmail.com>
2017-12-21 12:02:35 +0100

    Added PersonMap(Test)

---
+++ HelloNHibernate/Dao/PersonMap.cs
@@ -0,0 +1,16 @@
+﻿using HelloNHibernate.Model;
+using NHibernate.Mapping.ByCode;
+using NHibernate.Mapping.ByCode.Conformist;
+
+namespace HelloNHibernate.Dao
+{
+    public class PersonMap : ClassMapping<Person>
+    {
+        public PersonMap()
+        {
+            Id(x => x.Id, m => m.Generator(Generators.GuidComb));
+            Property(x => x.FirstName);
+            Property(x => x.LastName);
+        }
+    }
+}
---
+++ TestHelloNHibernate/PersonMapTest.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Xml.Serialization;
+using HelloNHibernate.Dao;
+using NHibernate.Mapping.ByCode;
+using System.IO;
+
+namespace TestHelloNHibernate
+{
+    [TestClass]
+    public class PersonMapTest
+    {
+        //[TestMethod] // disabled because it takes 1 minute and has no Assert, but still shows the usage of ModelMapper
+        public void CanGenerateXmlMapping()
+        {
+            var mapper = new ModelMapper();
+            mapper.AddMapping<PersonMap>();
+
+            var mapping = mapper.CompileMappingForAllExplicitlyAddedEntities();
+            var xmlSerializer = new XmlSerializer(mapping.GetType());
+
+            StringWriter sw = new StringWriter();
+            xmlSerializer.Serialize(sw, mapping);
+            string result = sw.ToString();
+        }
+    }
+}
