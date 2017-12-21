35f84ecd846dbc43897797ffffa9ed40f3aa7790
Tama McGlinn <t.mcglinn@gmail.com>
2017-12-21 12:26:00 +0100

    Added NHibernatePersonRepository (and interface) (tests)

---
+++ HelloNHibernate/Model/NHibernatePersonRepository.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using HelloNHibernate.Model;
+using NHibernate;
+
+namespace HelloNHibernate.Dao
+{
+    public class NHibernatePersonRepository : IPersonRepository
+    {
+        public void Save(Person person)
+        {
+            using (ISession session = NHibernateHelper.OpenSession())
+            using (ITransaction transaction = session.BeginTransaction())
+            {
+                session.Save(person);
+                transaction.Commit();
+            }
+        }
+
+        public Person Get(Guid id)
+        {
+            using (ISession session = NHibernateHelper.OpenSession())
+                return session.Get<Person>(id);
+        }
+
+        public void Update(Person person)
+        {
+            using (ISession session = NHibernateHelper.OpenSession())
+            using (ITransaction transaction = session.BeginTransaction())
+            {
+                session.Update(person);
+                transaction.Commit();
+            }
+        }
+
+        public void Delete(Person person)
+        {
+            using (ISession session = NHibernateHelper.OpenSession())
+            using (ITransaction transaction = session.BeginTransaction())
+            {
+                session.Delete(person);
+                transaction.Commit();
+            }
+        }
+
+        public long RowCount()
+        {
+            using (ISession session = NHibernateHelper.OpenSession())
+            {
+                return session.QueryOver<Person>().RowCountInt64();
+            }
+        }
+    }
+}
---
+++ TestHelloNHibernate/NHibernateRepositoryTest.cs
@@ -0,0 +1,75 @@
+﻿using System.IO;
+using HelloNHibernate.Model;
+using NHibernate.Tool.hbm2ddl;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HelloNHibernate.Dao.Test
+{
+    [TestClass]
+    public class NHibernatePersonRepositoryTest
+    {
+        private IPersonRepository _personRepo;
+
+        [TestInitialize]
+        public void CreateSchema()
+        {
+            DeleteDatabaseIfExists();
+
+            var schemaUpdate = new SchemaUpdate(NHibernateHelper.Configuration);
+            schemaUpdate.Execute(false, true);
+
+            _personRepo = new NHibernatePersonRepository();
+        }
+
+        [TestMethod]
+        public void CanSavePerson()
+        {
+            _personRepo.Save(new Person());
+            Assert.AreEqual(1, _personRepo.RowCount());
+        }
+
+        [TestMethod]
+        public void CanGetPerson()
+        {
+            var person = new Person();
+            _personRepo.Save(person);
+            Assert.AreEqual(1, _personRepo.RowCount());
+
+            person = _personRepo.Get(person.Id);
+            Assert.IsNotNull(person);
+        }
+
+        [TestMethod]
+        public void CanUpdatePerson()
+        {
+            var person = new Person();
+            _personRepo.Save(person);
+            Assert.AreEqual(1, _personRepo.RowCount());
+
+            person = _personRepo.Get(person.Id);
+            person.FirstName = "Test";
+            _personRepo.Update(person);
+
+            Assert.AreEqual(1, _personRepo.RowCount());
+            Assert.AreEqual("Test", _personRepo.Get(person.Id).FirstName);
+        }
+
+        [TestMethod]
+        public void CanDeletePerson()
+        {
+            var person = new Person();
+            _personRepo.Save(person);
+            Assert.AreEqual(1, _personRepo.RowCount());
+
+            _personRepo.Delete(person);
+            Assert.AreEqual(0, _personRepo.RowCount());
+        }
+
+        [TestCleanup]
+        public void DeleteDatabaseIfExists()
+        {
+            if (File.Exists("test.db"))
+                File.Delete("test.db");
+        }
+    }
+}
