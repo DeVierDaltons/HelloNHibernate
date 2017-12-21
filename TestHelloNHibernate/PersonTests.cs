1f3c2527a8b79fac0604004b1bf7774a9c3f629b
Tama McGlinn <t.mcglinn@gmail.com>
2017-12-21 11:46:40 +0100

    Added TestHelloNHibernate project with PersonTests

---
+++ TestHelloNHibernate/PersonTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HelloNHibernate.Model;
+
+namespace TestHelloNHibernate
+{
+    [TestClass]
+    public class PersonTests
+    {
+        [TestMethod]
+        public void GetFullNameTest()
+        {
+            var person = new Person
+            {
+                FirstName = "Test",
+                LastName = "Kees"
+            };
+
+            Assert.AreEqual("Test", person.FirstName);
+            Assert.AreEqual("Kees", person.LastName);
+
+            Assert.AreEqual("Test Kees", person.GetFullName());
+        }
+    }
+}
---
+++ TestHelloNHibernate/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestHelloNHibernate")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Ministerie van Defensie")]
+[assembly: AssemblyProduct("TestHelloNHibernate")]
+[assembly: AssemblyCopyright("Copyright © Ministerie van Defensie 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("abbda113-9439-448c-960b-3a55c46f44ba")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
