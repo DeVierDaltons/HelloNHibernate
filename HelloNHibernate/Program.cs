7b2fc8c57f6f69de705dafe9073e65e39e8d9217
Olaf van der Kruk <olijf@users.noreply.github.com>
2018-01-02 14:59:15 +0100

    HelloNHibernateFirstTry

--- HelloNHibernate/Program.cs
+++ HelloNHibernate/Program.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using NHibernate.Cfg;
+using NHibernate.Dialect;
+using NHibernate.Driver;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +14,46 @@ namespace HelloNHibernate
     {
         static void Main(string[] args)
         {
+            var cfg = new Configuration();
+            String ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=NHibernateTestDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            cfg.DataBaseIntegration(x =>
+            {
+                x.ConnectionString = ConnectionString;
+                x.Driver<SqlClientDriver>();
+                x.Dialect<MsSql2008Dialect>();
+            });
+            cfg.AddAssembly(Assembly.GetExecutingAssembly());
+
+            var sefact = cfg.BuildSessionFactory();
+            using (var session = sefact.OpenSession())
+            {
+
+                using (var tx = session.BeginTransaction())
+                {
+
+                    var student1 = new Product
+                    {
+                        Category = "Blha",
+                        Name = "blah",
+                        Id = new Guid()
+                        
+
+                    };
+
+                    var student2 = new Product
+                    {
+                        Category = "Glah",
+                        Name = "Konijn",
+                        Id = new Guid()
+                    };
+
+                    session.Save(student1);
+                    session.Save(student2);
+                    tx.Commit();
+                }
+
+                Console.ReadLine();
+            }
         }
     }
 }
