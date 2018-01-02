c2f5833443dc2fb47b2031b004ec2b7b22f143f0
Olaf van der Kruk <olijf@users.noreply.github.com>
2018-01-02 16:18:11 +0100

    Can do CR
    still work on UD

--- HelloNHibernate/Program.cs
+++ HelloNHibernate/Program.cs
@@ -1,6 +1,8 @@
 ﻿using NHibernate;
 using NHibernate.Cfg;
 using NHibernate.Tool.hbm2ddl;
+using System;
+using System.Linq;
 
 namespace HelloNHibernate
 {
@@ -12,20 +14,45 @@ namespace HelloNHibernate
             ISession session = NHibernateHelper.SessionFactory.OpenSession();
             try
             {
+                /*
                 using (ITransaction tx = session.BeginTransaction())
                 {
                     for (int i = 0; i < 30; i++)
                     {
+                        var employee = new Employee { Name = new Name { FirstName = "Employee", LastName = string.Format("{0}", i) } };
+                        var employer = new Employer { Name = string.Format("Employer {0}", i) };
                         var employment = new Employment
                         {
-                            Employee = new Employee { Name = new Name { FirstName = "Employee", LastName = string.Format("{0}",i) } },
-                            Employer = new Employer { Name = string.Format("Employer {0}", i)},
+                            Employee = employee,
+                            Employer = employer,
                             StartDate = new System.DateTime(),
                             EndDate = new System.DateTime().AddDays(i)
-
                         };
+                        session.Save(employee);
+                        session.Save(employer);
                         session.Save(employment);
                     }
+                    tx.Commit();
+                }
+                using (var tx = session.BeginTransaction())
+                {
+                    var employments = session.Query<Employment>().Where(w => w.Employee.Name.FirstName == "Employee").ToList();
+                    foreach (var employee in employments)
+                    {
+                        Console.Out.WriteLine("Employee: " + employee.Employee.Name.LastName);
+                    }
+
+                    tx.Commit();
+                }
+                */
+                using (var tx = session.BeginTransaction())
+                {
+                    var employments = session.Query<Employment>().ToList();
+                    foreach (var employment in employments)
+                    {
+                        Console.Out.WriteLine("Employee: " + employment.Employee.Name.LastName + " end date " + employment.EndDate + " at employer " + employment.Employer.Name);
+                    }
+
                     tx.Commit();
                 }
             }
